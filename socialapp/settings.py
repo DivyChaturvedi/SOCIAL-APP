c40b0a174f8f1ad134ca61f8ec0e125ea2a76c12
Divy Chaturvedi <divychaturvedi990@gmail.com>
2025-11-04 13:07:24 +0530

    final render config fix

--- socialapp/settings.py
+++ socialapp/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['social-app-002e.onrender.com', 'localhost', '127.0.0.1']
 
