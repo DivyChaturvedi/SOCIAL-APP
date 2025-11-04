554cb30bc7228cc3522440d6adf5fd1e177b987e
Divy Chaturvedi <divychaturvedi990@gmail.com>
2025-11-04 13:01:49 +0530

    fix static and allowed hosts

--- socialapp/settings.py
+++ socialapp/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['social-app-002e.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
